c955f3f695367a718b0f0d002b6725b294cd147e
Suthanth D K <suthuganesha.dk@gmail.com>
2019-03-17 21:45:17 +0530

    Enabled django admin

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -6,25 +6,25 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserProfileManager(BaseUserManager):
     """Helps django to work with our custom user model """
-    def create_user(self, email, name , password=None):
+    def create_user(self, email, name, password=None):
         if not email:
             raise ValueError('User must have email')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, name, password):
+    def create_superuser(self, email,  password, name='mine',):
         """creates the super user with given details"""
 
         user = self.create_user(email, name, password)
         user.is_superuser = True
         user.is_staff = True
-        User.save(using=self._db)
+        user.save(using=self._db)
 
         return user
 
@@ -39,7 +39,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    # REQUIRED_FIELD = ['name']
 
     def get_full_name(self):
         return self.name
