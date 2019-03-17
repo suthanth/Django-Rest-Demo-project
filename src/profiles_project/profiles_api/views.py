60d6a62842b72729968a0e2c47aa4c2eacddf156
Suthanth D K <suthuganesha.dk@gmail.com>
2019-03-18 00:42:00 +0530

    Added first Api view - Hello API View

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class HelloApiView(APIView):
+    """Test Api View."""
+
+    def get(self, request, format=None):
+        """Returns api view format"""
+
+        an_apiview = [
+            'Http method',
+            'Test APi view'
+        ]
+
+        return Response({'message':'Hello', 'an_apiview':an_apiview})
