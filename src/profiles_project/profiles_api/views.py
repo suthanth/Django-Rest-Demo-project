9a9afb356b187d49416f50defab1485c94889f9e
Suthanth D K <suthuganesha.dk@gmail.com>
2019-03-19 00:43:34 +0530

    Added serializer and added post,put,patch, delete requests to HelloApiView

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from . import serializers
 # Create your views here.
 class HelloApiView(APIView):
     """Test Api View."""
+
+    serialzer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns api view format"""
@@ -15,3 +19,32 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message':'Hello', 'an_apiview':an_apiview})
+
+    def post(self, request):
+        """Creates serialized object"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = "Hello {0}".format(name)
+            return Response({'message':message})
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, pk=None):
+        """Updates an object"""
+
+        return Response({"method":"put"})
+
+    def patch(self, request, pk=None):
+        """Updates the specified fields"""
+
+        return Response({"method":"patch"})
+
+    def delete(self, request,pk=None):
+        """Deletes an object"""
+
+        return Response({'method':'delete'})
